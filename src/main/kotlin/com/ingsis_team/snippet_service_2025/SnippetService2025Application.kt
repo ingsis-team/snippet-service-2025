9705cbf06b45ca4909ccefb14a22503100412d4a
Martina <martina.dejesus@despegar.com>
2025-09-30 20:11:04 -0300

    first commit

---
+++ src/main/kotlin/com/ingsis_team/snippet_service_2025/SnippetService2025Application.kt
@@ -0,0 +1,11 @@
+package com.ingsis_team.snippet_service_2025
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetService2025Application
+
+fun main(args: Array<String>) {
+	runApplication<SnippetService2025Application>(*args)
+}
---
+++ src/test/kotlin/com/ingsis_team/snippet_service_2025/SnippetService2025ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ingsis_team.snippet_service_2025
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetService2025ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
