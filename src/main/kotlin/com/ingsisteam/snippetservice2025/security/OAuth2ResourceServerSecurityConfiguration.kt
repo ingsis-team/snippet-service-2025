7ad0e448333eb7869543c5bb45bd22ec7f693189
tomasalcazar <tomas.alcazar@ing.austral.edu.ar>
2025-11-13 17:01:31 -0300

    Remove unused import statement in OAuth2ResourceServerSecurityConfiguration to improve code cleanliness and maintainability.

--- src/main/kotlin/com/ingsisteam/snippetservice2025/security/OAuth2ResourceServerSecurityConfiguration.kt
+++ src/main/kotlin/com/ingsisteam/snippetservice2025/security/OAuth2ResourceServerSecurityConfiguration.kt
@@ -1,7 +1,6 @@
 package com.ingsisteam.snippetservice2025.security
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer.withDefaults
